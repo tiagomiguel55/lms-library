6d460ecc80558b0a3f9f00eb9ea275819cfe9cc7
tiagomiguel55 <1250554@isep.ipp.pt>
2025-12-29 18:02:16 +0000

    feat: enable scheduling support in LMSBooks application

--- P2/lms_books_command/src/main/java/pt/psoft/g1/psoftg1/LMSBooks.java
+++ P2/lms_books_command/src/main/java/pt/psoft/g1/psoftg1/LMSBooks.java
@@ -2,8 +2,10 @@ package pt.psoft.g1.psoftg1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LMSBooks {
 
     public static void main(String[] args) {
