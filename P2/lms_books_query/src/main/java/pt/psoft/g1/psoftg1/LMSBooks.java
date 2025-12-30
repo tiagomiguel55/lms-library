5910b87fb14566d24c4acd9c6e6bd39245b96c9b
tiagomiguel55 <1250554@isep.ipp.pt>
2025-12-30 13:18:39 +0000

    enable MongoDB repositories for book, author, genre, and shared management

--- P2/lms_books_query/src/main/java/pt/psoft/g1/psoftg1/LMSBooks.java
+++ P2/lms_books_query/src/main/java/pt/psoft/g1/psoftg1/LMSBooks.java
@@ -2,8 +2,16 @@ package pt.psoft.g1.psoftg1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = {
+    "pt.psoft.g1.psoftg1.bookmanagement.infrastructure.repositories.impl",
+    "pt.psoft.g1.psoftg1.authormanagement.infrastructure.repositories.impl",
+    "pt.psoft.g1.psoftg1.genremanagement.infrastructure.repositories.impl",
+    "pt.psoft.g1.psoftg1.bookmanagement.repositories",
+    "pt.psoft.g1.psoftg1.shared.infrastructure.repositories.impl"
+})
 public class LMSBooks {
 
     public static void main(String[] args) {
