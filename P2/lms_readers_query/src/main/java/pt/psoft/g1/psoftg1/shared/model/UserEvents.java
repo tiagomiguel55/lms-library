459209a93ca2100289a774c26a213e87e2846631
DiogoAntunes <1211165@isep.ipp.pt>
2025-12-31 17:15:58 +0000

    lms_readers_query - AMQP implementation for Users

---
+++ P2/lms_readers_query/src/main/java/pt/psoft/g1/psoftg1/shared/model/UserEvents.java
@@ -0,0 +1,8 @@
+package pt.psoft.g1.psoftg1.shared.model;
+
+public interface UserEvents {
+    String USER_CREATED = "user.created";
+    String USER_UPDATED = "user.updated";
+    String USER_DELETED = "user.deleted";
+    String USER_REQUESTED = "reader.user.requested.user";
+}
