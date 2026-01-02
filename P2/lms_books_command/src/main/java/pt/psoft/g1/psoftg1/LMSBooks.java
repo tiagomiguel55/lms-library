3d1461930ba00a9bb8f77004a45eda6b8db9be46
tiagomiguel55 <1250554@isep.ipp.pt>
2026-01-02 14:35:19 +0000

    add testing comment

--- P2/lms_books_command/src/main/java/pt/psoft/g1/psoftg1/LMSBooks.java
+++ P2/lms_books_command/src/main/java/pt/psoft/g1/psoftg1/LMSBooks.java
@@ -3,7 +3,7 @@ package pt.psoft.g1.psoftg1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+// testing comment
 @SpringBootApplication
 @EnableScheduling
 public class LMSBooks {
