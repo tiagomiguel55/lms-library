dfc8a099ff0c2bd0564f91211be630fb49253b67
tiagomiguel55 <1250554@isep.ipp.pt>
2026-01-03 12:53:47 +0000

    remove test comment from WebMvcConfig.java

--- P2/lms_books_command/src/main/java/pt/psoft/g1/psoftg1/configuration/WebMvcConfig.java
+++ P2/lms_books_command/src/main/java/pt/psoft/g1/psoftg1/configuration/WebMvcConfig.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//test
+
 /**
  * Web MVC Configuration to register feature flag interceptor
  */
